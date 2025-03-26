fbfbe5fda8af432a823e2633fbce75596c169586
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-03-26 20:03:52 -0300

    Update text_button.py

--- geeks_to_geeks_ubuntu/tkinter_trainer/text_button.py
+++ geeks_to_geeks_ubuntu/tkinter_trainer/text_button.py
@@ -1,14 +1,16 @@
 import tkinter as tk
+from tkinter import ttk
 
 root = tk.Tk()
 
-def change():
-    label.config(text="Jesus gave his life for you!")
+root.geometry("500x500")
 
-root.geometry('500x500')
-label = tk.Label(root, text="Jesus is the light of the World!")
+def change():
+    label.config(text="Jesus love we, although we don't deserve... He loves we")
+
+label = tk.Label(root, text="Jesus is the light of the world!")
 label.pack()
-button = tk.Button(root, text="Click me", command=change)
+button = tk.Button(root, text="clik here", command=change)
 button.pack()
 
 root.mainloop()
