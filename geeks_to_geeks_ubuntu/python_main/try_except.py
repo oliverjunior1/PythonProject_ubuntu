d6c0fd4e5c2994bb4230d4bd44a91b5784723fb4
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-03-27 17:16:20 -0300

    Update try_except.py

--- geeks_to_geeks_ubuntu/python_main/try_except.py
+++ geeks_to_geeks_ubuntu/python_main/try_except.py
@@ -1,6 +1,5 @@
 try:
-    x = 10*500/0
+    x = 150/0
     print(x)
 except:
-    print("You can't divide a number to zero!")
-    print("Intent again when you've learned math!")
+    print("You can't divide a number to zero.")
