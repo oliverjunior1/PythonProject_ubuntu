c5ec70c79b2b2e2a77716c60bd1500f59ccaad9a
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-03-27 18:40:27 -0300

    Update list_comprehension.py

--- geeks_to_geeks_ubuntu/python_main/list_comprehension.py
+++ geeks_to_geeks_ubuntu/python_main/list_comprehension.py
@@ -1,5 +1,11 @@
-x = [1,2,3,4,5,6,7,8]
+# x = [1,2,3,4,5,6,7,8]
+#
+# y = [a**2 for a in x]
+#
+# print(y)
 
-y = [a**2 for a in x]
+fruits = ["apple", "lemon", "pineapple", 'melon', 'grape']
 
-print(y)
+fruits_a = [x for x in fruits if 'a' in x]
+
+print(fruits_a)
