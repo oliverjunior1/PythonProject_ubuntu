e34e2572209eff7c2b6003a8c47a8caa097b64d4
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-03-27 19:16:23 -0300

    Create list_tupples_dict_sets_array.py

---
+++ geeks_to_geeks_ubuntu/python_main/list_tupples_dict_sets_array.py
@@ -0,0 +1,28 @@
+# list
+my_list = ['apple', 'melon', 'orange']
+
+print(my_list)
+print(type(my_list))
+
+# tupple
+my_tupple = ('apple', 'melon', 'orange')
+
+print(my_tupple)
+print(type(my_tupple))
+
+# set
+my_set = {'apple', 'melon', 'orange'}
+
+print(my_set)
+print(type(my_set))
+
+# dict
+my_dict = {1:'Bible', 2:'Footbal to Idiots'}
+
+print(my_dict)
+print(type(my_dict))
+
+
+
+
+
