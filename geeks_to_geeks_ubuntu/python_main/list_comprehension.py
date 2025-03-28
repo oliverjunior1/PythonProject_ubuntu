a7be231b4be24451c887688d0df5167e1e988aa4
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-03-28 17:07:14 -0300

    Update list_comprehension.py

--- geeks_to_geeks_ubuntu/python_main/list_comprehension.py
+++ geeks_to_geeks_ubuntu/python_main/list_comprehension.py
@@ -1,11 +1,11 @@
 # x = [1,2,3,4,5,6,7,8]
 #
-# y = [a**2 for a in x]
+# y = [(a**2)/5 for a in x]
 #
 # print(y)
 
-fruits = ["apple", "lemon", "pineapple", 'melon', 'grape']
+names = ["Joao", "Joaquim", "Alyne", "José", "Davelina", "Zélia"]
 
-fruits_a = [x for x in fruits if 'a' in x]
+names_j = [x for x in names if "J" in x]
 
-print(fruits_a)
+print(names_j)
