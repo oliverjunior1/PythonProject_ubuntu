faad65539294f03b6916c42f6b857a3642192b3d
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-03-28 16:12:39 -0300

    Update match-case.py

--- geeks_to_geeks_ubuntu/python_main/match-case.py
+++ geeks_to_geeks_ubuntu/python_main/match-case.py
@@ -1,11 +1,29 @@
-username = input('Enter username:')
+# username = input('Enter username:')
+#
+# match username:
+#     case 'John':
+#         print("Welcome Admin")
+#     case 'sim':
+#         print("Welcome User")
+#     case 'spongy':
+#         print('Welcome Guest')
+#     case _:
+#         print('Invalid username')
 
-match username:
-    case 'John':
-        print("Welcome Admin")
-    case 'sim':
-        print("Welcome User")
-    case 'spongy':
-        print('Welcome Guest')
-    case _:
-        print('Invalid username')
+day = int(input('Enter the number of the day in week:'))
+
+match day:
+    case 1:
+        print('sunday')
+    case 2:
+        print('monday')
+    case 3:
+        print('tuesday')
+    case 4:
+        print('wednesday')
+    case 5:
+        print('thursday')
+    case 6:
+        print('friday')
+    case 7:
+        print('')
