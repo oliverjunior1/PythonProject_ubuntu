92c9944d89298c47e22f913791ba4f73c44b3c4d
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-03-29 17:40:46 -0300

    Update list_comprehension.py

--- geeks_to_geeks_ubuntu/python_main/list_comprehension.py
+++ geeks_to_geeks_ubuntu/python_main/list_comprehension.py
@@ -1,12 +1,11 @@
 # x = [1,2,3,4,5,6,7,8]
 #
-# y = [(a**2)/5 for a in x]
+# y = [a**3 for a in x]
 #
 # print(y)
 
-names = ["Joao", "Joaquim", "Alyne", "José", "Davelina", "Zélia"]
+fruits = ['apple', 'lemon', 'pineapple', 'orange', 'grape', 'melon']
 
-search_letter = input("Put a letter to search in names: ").capitalize()
-names_j = [x for x in names if f"{search_letter}" in x]
+fruits_a = [x for x in fruits if 'a' in x]
 
-print(names_j)
+print(fruits_a)
