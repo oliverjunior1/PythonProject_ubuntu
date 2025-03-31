299a3e609e7bf574a952e65aa30b93517c267e2d
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-03-31 19:24:12 -0300

    Update list_tupples_dict_sets_array.py

--- geeks_to_geeks_ubuntu/python_main/list_tupples_dict_sets_array.py
+++ geeks_to_geeks_ubuntu/python_main/list_tupples_dict_sets_array.py
@@ -1,39 +1,27 @@
 import pandas as pd
+
 # list
-print('\nlist')
 my_list = [1,2,3,4,5]
 
-print(my_list)
 print(type(my_list))
 
 # tuple
-print('\ntuple')
 my_tuple = (1,2,3,4,5)
 
-print(my_tuple)
 print(type(my_tuple))
 
 # dict
-print('\ndict')
-my_dict = {'apples':10, 'oranges':25, 'melons':15}
+my_dict = {'fruits':'apple', 'name':'Alyne'}
 
-print(my_dict)
 print(type(my_dict))
 
 # set
-print('\nset')
 my_set = {1,2,3,4,5}
 
-print(my_set)
 print(type(my_set))
 
 # array
-print('\narray')
 my_array = pd.array([1,2,3,4,5])
 
-print(my_array)
 print(type(my_array))
-
-
-
 
