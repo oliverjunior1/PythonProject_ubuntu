4829bef9c13846f72ce17cb2863739a675d2cd05
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-01 17:22:04 -0300

    Update open_documents_with_python.py

--- geeks_to_geeks_ubuntu/python_main/open_documents_with_python.py
+++ geeks_to_geeks_ubuntu/python_main/open_documents_with_python.py
@@ -2,3 +2,12 @@ x = open('message.txt', 'r')
 y = x.read()
 
 print(y)
+
+with open('message2.txt', 'w') as file:
+    file.write('Jesus is the way, the truth and the life.\n')
+
+a = open('message2.txt', 'r')
+b = a.read()
+
+print(b)
+
