cee4749e2d72665cf91ec062e5882c48ffbb538d
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-01 19:18:13 -0300

    Update lambda_test.py

--- geeks_to_geeks_ubuntu/a_python_main/lambda_test.py
+++ geeks_to_geeks_ubuntu/a_python_main/lambda_test.py
@@ -1,5 +1,7 @@
-quadrate = lambda a, b, c: (a**b)//c
+# lambda 3x
+side_one = int(input('Enter the side one:'))
+side_two = int(input('Enter the side two:'))
 
-print(quadrate(5,5,5))
-print(quadrate(5,4,20))
-print(quadrate(25,2,100))
+rect_area = lambda a, b: a * b
+
+print(rect_area(side_one, side_two))
