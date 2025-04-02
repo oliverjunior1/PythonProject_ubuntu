7198464eb4da4d050833adc72a792932efdf7a03
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-01 21:00:30 -0300

    Update open_documents_with_python.py

--- geeks_to_geeks_ubuntu/a_python_main/open_documents_with_python.py
+++ geeks_to_geeks_ubuntu/a_python_main/open_documents_with_python.py
@@ -1,13 +1,11 @@
 x = open('message.txt', 'r')
 y = x.read()
-
 print(y)
 
 with open('message2.txt', 'w') as file:
-    file.write('Jesus is the way, the truth and the life.\n')
+    file.write("God has been love the world, at some mode, who give your only son, for everybody who believes don't pain, but had the eternal life.")
 
 a = open('message2.txt', 'r')
 b = a.read()
 
 print(b)
-
