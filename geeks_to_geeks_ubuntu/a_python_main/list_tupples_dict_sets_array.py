694f368d495d0974264af93b255e2d3ac8d5c818
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-03 20:01:28 -0300

    Update list_tupples_dict_sets_array.py

--- geeks_to_geeks_ubuntu/a_python_main/list_tupples_dict_sets_array.py
+++ geeks_to_geeks_ubuntu/a_python_main/list_tupples_dict_sets_array.py
@@ -1,22 +1,25 @@
+# four numbers
 import pandas as pd
 
-my_list = [1,2,3,4,5]
+my_list = [1,2,3,4]
 print(type(my_list))
 
 
-my_tuple = (1,2,3,4,5)
+my_tuple = (1,2,3,4)
 print(type(my_tuple))
 
 
-my_dict = {'a':1, 'b':2}
+my_dict = {'a':1,'b':2, 'c':3, 'd':4}
 print(type(my_dict))
 
 
-my_list = [1,2,3,4,5]
-print(type(my_list))
+my_set = {1,2,3,4}
+print(type(my_set))
 
 
-my_list = [1,2,3,4,5]
-print(type(my_list))
+my_array = pd.array([1,2,3,4])
+print(type(my_array))
+
+
 
     
