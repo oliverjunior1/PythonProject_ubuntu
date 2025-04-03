9777f22fa0e99ee82ff4f8faed61eb38e522fa0a
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-03 20:46:43 -0300

    Update open_documents_with_python.py

--- geeks_to_geeks_ubuntu/a_python_main/open_documents_with_python.py
+++ geeks_to_geeks_ubuntu/a_python_main/open_documents_with_python.py
@@ -1,11 +1,12 @@
+
 x = open('message.txt', 'r')
 y = x.read()
+
 print(y)
 
 with open('message2.txt', 'w') as file:
-    file.write("God has been love the world, at some mode, who give your only son, for everybody who believes don't pain, but had the eternal life.")
+    file.write('Jesus is the king of the kings and the Lord of the Lords.')
 
-a = open('message2.txt', 'r')
-b = a.read()
-
-print(b)
+x = open('message2.txt', 'r')
+y = x.read()
+print(y)
