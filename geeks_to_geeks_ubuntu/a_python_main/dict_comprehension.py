95f332968a6db1540e791151ca32946099d001ad
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-03 19:29:02 -0300

    Update dict_comprehension.py

--- geeks_to_geeks_ubuntu/a_python_main/dict_comprehension.py
+++ geeks_to_geeks_ubuntu/a_python_main/dict_comprehension.py
@@ -1,7 +1,7 @@
-# dict comprehension with five items
-x = ['a', 'b', 'c', 'd', 'e']
-y = [1,2,3,4,5]
+# dict comprehension with four items
+x = ['a', 'b', 'c', 'd']
+y = [1,2,3,4]
 
-my_dict = {a:b for (a,b) in zip(x,y)}
+my_dict = {a:b for (a, b) in zip(x,y)}
 
 print(my_dict)
