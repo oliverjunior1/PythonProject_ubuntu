b1dfcca811e40bbf15464394b3700a99a44a3e0d
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-03 19:50:23 -0300

    Update lambda_test.py

--- geeks_to_geeks_ubuntu/a_python_main/lambda_test.py
+++ geeks_to_geeks_ubuntu/a_python_main/lambda_test.py
@@ -1,4 +1,10 @@
 #Problem area of the rectangle
 # lambda 3x
 
-side1 = int()
+side1 = float(input("Put the side one:"))
+side2 = float(input("Put the side two:"))
+
+
+rect = lambda x, y: x*y
+
+print(f'The are of the rectangle is: {rect(side1, side2)} m2!')
