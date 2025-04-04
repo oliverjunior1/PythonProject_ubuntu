00237fc422d94ae820511cfe4fc8196866d1eee2
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-04 18:09:50 -0300

    Update open_documents_with_python.py

--- geeks_to_geeks_ubuntu/a_python_main/open_documents_with_python.py
+++ geeks_to_geeks_ubuntu/a_python_main/open_documents_with_python.py
@@ -2,3 +2,10 @@
 x = open('message.txt', 'r')
 y = x.read()
 print(y)
+
+with open('message2.txt', 'w') as file:
+    file.write("Jesus is the salt of the earth and the light of the World!")
+
+a = open('message2.txt', 'r')
+b = a.read()
+print(b)
