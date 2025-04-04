c1967a2d4373d317bfdd401084ce3e7006a61724
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-04 18:02:08 -0300

    Update open_documents_with_python.py

--- geeks_to_geeks_ubuntu/a_python_main/open_documents_with_python.py
+++ geeks_to_geeks_ubuntu/a_python_main/open_documents_with_python.py
@@ -1,12 +1,1 @@
 
-x = open('message.txt', 'r')
-y = x.read()
-
-print(y)
-
-with open('message2.txt', 'w') as file:
-    file.write('Jesus is the king of the kings and the Lord of the Lords.')
-
-x = open('message2.txt', 'r')
-y = x.read()
-print(y)
