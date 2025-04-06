450c74b819a5288cd25012d38643bbbfda75f139
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-05 21:08:25 -0300

    Update open_documents_with_python.py

--- geeks_to_geeks_ubuntu/a_python_main/open_documents_with_python.py
+++ geeks_to_geeks_ubuntu/a_python_main/open_documents_with_python.py
@@ -1,12 +1,17 @@
 # read the text and write the text
+# new text a)"He's called Emmanuel, and he brought peace to this world!"
+# new text b)"God loved the world, from that manner, who give his son, for everybody who believe in him, don't perish, but have the eternal life."
+
 
 x = open('message.txt', 'r')
-y =x.read()
+y = x.read()
 print(y)
 
 with open('message2.txt', 'w') as file:
-    file.write('Jesus give his own life for us sinners...')
+    file.write("He's called Emmanuel, and he brought peace to this world!")
 
 a = open('message2.txt', 'r')
 b = a.read()
+
 print(b)
+
