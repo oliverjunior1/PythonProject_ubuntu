13c7e5a71771027283a2c712ac2e2cb973ad074d
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-06 15:10:22 -0300

    Update display_images_using_pygame.py

--- geeks_to_geeks_ubuntu/pygame_trainer/display_images_using_pygame.py
+++ geeks_to_geeks_ubuntu/pygame_trainer/display_images_using_pygame.py
@@ -7,6 +7,9 @@ pygame.init()
 
 screen = pygame.display.set_mode((500,500))
 
+# draw
+draw = pygame.draw.rect(screen, 'red', (50,50,30,30))
+
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
