d49189464f42bf2e82e7b1049ae1b2ec5f55c380
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-05 22:00:28 -0300

    Update c_pie_trainer.py

--- geeks_to_geeks_ubuntu/matplotlib_trainer/c_pie_trainer.py
+++ geeks_to_geeks_ubuntu/matplotlib_trainer/c_pie_trainer.py
@@ -3,5 +3,6 @@ import matplotlib.pyplot as plt
 x = ['Python', 'Javascript', 'Java', 'C#', 'Outros']
 y = [30,20,15,10,25]
 
+plt.title('World Most Used Languages')
 plt.pie(y, labels=x, autopct='%0.00f%%')
 plt.show()
