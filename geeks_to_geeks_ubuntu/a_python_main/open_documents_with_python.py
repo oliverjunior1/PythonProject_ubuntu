fd8195407c33bb12516033b58a6363df8b31ebc4
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-05 21:13:30 -0300

    Update open_documents_with_python.py

--- geeks_to_geeks_ubuntu/a_python_main/open_documents_with_python.py
+++ geeks_to_geeks_ubuntu/a_python_main/open_documents_with_python.py
@@ -2,16 +2,13 @@
 # new text a)"He's called Emmanuel, and he brought peace to this world!"
 # new text b)"God loved the world, from that manner, who give his son, for everybody who believe in him, don't perish, but have the eternal life."
 
-
 x = open('message.txt', 'r')
 y = x.read()
 print(y)
 
 with open('message2.txt', 'w') as file:
-    file.write("He's called Emmanuel, and he brought peace to this world!")
+    file.write("God loved the world, from that manner, who give his son, for everybody who believe in him, don't perish, but have the eternal life.")
 
 a = open('message2.txt', 'r')
 b = a.read()
-
 print(b)
-
