c293473e6b7ce6abf53eebfb59fa929d6b59759b
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-06 15:40:11 -0300

    Update display_images_using_pygame.py

--- geeks_to_geeks_ubuntu/pygame_trainer/display_images_using_pygame.py
+++ geeks_to_geeks_ubuntu/pygame_trainer/display_images_using_pygame.py
@@ -1,4 +1,4 @@
-# two screens
+# three screens
 
 import pygame
 import sys
@@ -10,9 +10,23 @@ screen = pygame.display.set_mode((500,500))
 # draw
 draw = pygame.draw.rect(screen, 'red', (50,50,30,30))
 
+# text
+font = pygame.font.SysFont('Times New Roman', 25)
+text = font.render("Jesus is the light of the World", True, "pink")
+text_rect = text.get_rect()
+text_rect.center=(250,250)
+
 while True:
+    screen.blit(text, text_rect)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_0:
+                screen.fill('lightblue')
+            if event.key==pygame.K_1:
+                screen.fill('green')
+            if event.key==pygame.K_2:
+                screen.fill('gray')
     pygame.display.flip()
