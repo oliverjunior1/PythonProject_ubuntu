66de093c975475cf678324b0191af55c4c1cf304
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-07 19:48:06 -0300

    Update a_introduction.py

--- geeks_to_geeks_ubuntu/PySimpleGUI_trainer/a_introduction.py
+++ geeks_to_geeks_ubuntu/PySimpleGUI_trainer/a_introduction.py
@@ -1,3 +1,12 @@
 import PySimpleGUI as sg
 
 sg.theme('SandyBeach')
+
+layout = [
+    [sg.Text('Please enter your name, age, Phone')],
+    [sg.Text('Name', size=(15,1)),sg.InputText()],
+    [sg.Text('Age', size=(15,1)), sg.InputText()],
+    [sg.Text('Phone', size=(15,1)),sg.InputText()],
+    [sg.Sudmit(), sg.Cancel()]
+]
+
