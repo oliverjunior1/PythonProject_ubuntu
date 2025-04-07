40453002f28ab30a8a62797753ae5cc4702f22e2
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-07 20:18:37 -0300

    Update text_button.py

--- geeks_to_geeks_ubuntu/tkinter_trainer/text_button.py
+++ geeks_to_geeks_ubuntu/tkinter_trainer/text_button.py
@@ -2,9 +2,14 @@ import tkinter as tk
 import tkinter as ttk
 
 root = tk.Tk()
+def text():
+    label.config(text='Jesus is the light of the World!')
 
 root.geometry('800x800')
 
-
+label = tk.Label(root, text="Jesus walk on the water, teach us how to have faith!")
+label.pack()
+button = ttk.Button(root, text="click here", command=text)
+button.pack()
 
 root.mainloop()
