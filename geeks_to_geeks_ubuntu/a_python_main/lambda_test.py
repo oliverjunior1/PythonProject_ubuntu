fcc65d4dd162ead342c675afc0309fc09f5b3e2a
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-08 18:42:21 -0300

    Update lambda_test.py

--- geeks_to_geeks_ubuntu/a_python_main/lambda_test.py
+++ geeks_to_geeks_ubuntu/a_python_main/lambda_test.py
@@ -1,8 +1,7 @@
-#Problem area of the rectangle:yes
-# lambda 2x
+# Problem of rectangle area: no
+# with input: no
+# lambda pow 3
 
-side_1 = int(input('Put the first side: '))
-side_2 = int(input('Put the second side: '))
+x = lambda a, b, c: a **b//c
 
-x = lambda a, b : a * b
-print(f"The rect area is {x(side_1, side_2)} m2.")
+print(x(2,6,6))
