697435fb3ea73e1199acc897bdda256339df41f0
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-08 17:55:13 -0300

    Update c_icon_photo.py

--- geeks_to_geeks_ubuntu/tkinter_trainer/c_icon_photo.py
+++ geeks_to_geeks_ubuntu/tkinter_trainer/c_icon_photo.py
@@ -1,23 +1,10 @@
-# Importing Tkinter module 
 from tkinter import *
 from tkinter.ttk import *
 
-# Creating master Tkinter window 
-master = Tk() 
+master = Tk()
 
-# Creating object of photoimage class 
-# Image should be in the same folder 
-# in which script is saved 
-p1 = PhotoImage(file = 'info.png')
-
-# Setting icon of master window 
-master.iconphoto(False, p1) 
-
-# Creating button 
-b = Button(master, text = 'Click me !') 
-b.pack(side = TOP) 
-
-# Infinite loop can be terminated by 
-# keyboard or mouse interrupt 
-# or by any predefined function (destroy()) 
-mainloop() 
+p1 = PhotoImage(file='bat.svg')
+master.iconphoto(False, p1)
+b = Button(master, text='Click me')
+b.pack(side=TOP)
+mainloop()
