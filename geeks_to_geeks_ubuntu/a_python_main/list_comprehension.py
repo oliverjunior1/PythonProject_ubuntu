687661c78f6c1a13e1d9fc2ef87eef622ff090c5
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-08 19:10:10 -0300

    Update list_comprehension.py

--- geeks_to_geeks_ubuntu/a_python_main/list_comprehension.py
+++ geeks_to_geeks_ubuntu/a_python_main/list_comprehension.py
@@ -12,5 +12,12 @@ fruits = ['apple', 'banana', 'bayberry', 'orange']
 fruits_b = [x for x in fruits if 'b' in x]
 
 print(fruits_b)
-# multiply all the elements from a list for 5
+# multiply all the elements from a list for 4
+
+x = [1,2,3,4,5,6,7,8,9,10]
+
+y = [a*4 for a in x]
+
+print(y)
+
 
