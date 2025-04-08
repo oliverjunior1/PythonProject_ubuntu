1e9e55bbaf5958ddf6938d8d34a9fa9c5fdb0c5f
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-08 17:45:45 -0300

    Update c_icon_photo.py

--- geeks_to_geeks_ubuntu/tkinter_trainer/c_icon_photo.py
+++ geeks_to_geeks_ubuntu/tkinter_trainer/c_icon_photo.py
@@ -2,4 +2,8 @@ from tkinter import *
 from tkinter.ttk import *
 
 root =Tk()
-p1 = PhotoImage(file='')
+p1 = PhotoImage(file='Untitled.jpeg')
+root.iconphoto(False, p1)
+b = Button(root, text='Click me!')
+b.pack(side=TOP)
+mainloop()
