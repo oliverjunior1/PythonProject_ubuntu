0e20b1de07e0659fc75b7ccf3f4e6adb16db569a
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-08 19:02:31 -0300

    Update list_comprehension.py

--- geeks_to_geeks_ubuntu/a_python_main/list_comprehension.py
+++ geeks_to_geeks_ubuntu/a_python_main/list_comprehension.py
@@ -1,25 +1,12 @@
-# four numbers  for 3
+# Three numbers  for 3
 # problem 1: Make a list with some family members, that change to a list with name and last name.
-from pygame.surfarray import pixels_red
+family = ['Alyne', 'Mariane', 'Joaquim', 'Joao']
 
-family = ["Joao ", "Mariane ", "Alyne ", "Joaquim "]
+family_ln = [x + " Oliveira" for x in family]
 
-f_oliveira = [x+'Oliveira' for x in family]
+print(family_ln)
 
-print(f_oliveira)
+# Withdraw from some list one list with only 'b'
 
-# Withdraw from some list one list with only 'a'
+# multiply all the elements from a list for 5
 
-fruits = ['banana', 'apple', 'melon', 'lemon', 'watermelon']
-
-fruits_a = [y for y in fruits if "a" in y]
-
-print(fruits_a)
-
-# multiply all the elements from a list for 4
-
-list = [10,20,30,40]
-
-list_for_4 = [z*4 for z in list]
-
-print(list_for_4)
