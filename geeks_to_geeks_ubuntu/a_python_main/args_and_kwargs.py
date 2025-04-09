71780d04396628719286832ab5b571cbecfda4af
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-09 18:04:42 -0300

    Update args_and_kwargs.py

--- geeks_to_geeks_ubuntu/a_python_main/args_and_kwargs.py
+++ geeks_to_geeks_ubuntu/a_python_main/args_and_kwargs.py
@@ -1,1 +1,7 @@
 # five numbers
+def args_and_kwargs(*args,  **kwargs):
+    print(f"args: {args}")
+    print(f"kwargs: {kwargs}")
+
+args_and_kwargs(1,2,3,4,5)
+args_and_kwargs(a=1, b=2, c=3, d=4, e=5)
