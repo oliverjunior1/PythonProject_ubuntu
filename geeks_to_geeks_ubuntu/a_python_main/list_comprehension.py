8a64c3be7fe7b810ea399cabac0613ee6e55bf42
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-09 18:33:09 -0300

    Update list_comprehension.py

--- geeks_to_geeks_ubuntu/a_python_main/list_comprehension.py
+++ geeks_to_geeks_ubuntu/a_python_main/list_comprehension.py
@@ -5,8 +5,12 @@ family = ["Joaquim", "Alyne", "Joao", "Mariane"]
 family_oliver = [x + " Oliveira" for x in family]
 
 print(family_oliver)
-# Withdraw from some list one list with only 'b'
+# Withdraw from some list one list with only 'c'
+fruits = ['cacao', 'orange', 'cherry', 'grape', 'crabapple']
 
+fruits_c = [x for x in fruits if 'c' in x]
+
+print(fruits_c)
 
 # multiply all the elements from a list for 5
 
