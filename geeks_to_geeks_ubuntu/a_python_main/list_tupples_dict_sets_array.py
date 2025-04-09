4d1d3226ce3f8f6f163ae6ba0d25d436900cf1b2
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-09 18:39:27 -0300

    Update list_tupples_dict_sets_array.py

--- geeks_to_geeks_ubuntu/a_python_main/list_tupples_dict_sets_array.py
+++ geeks_to_geeks_ubuntu/a_python_main/list_tupples_dict_sets_array.py
@@ -6,7 +6,7 @@ a = [1,2,3,4]
 print(type(a))
 
 b = (1,2,3,4)
-print(b)
+print(type(b))
 
 c = {'a':1, 'b':2, 'c':3, 'd':4}
 print(type(c))
@@ -14,3 +14,5 @@ print(type(c))
 d = {1,2,3,4}
 print(type(d))
 
+e = pd.array([1,2,3,4])
+print(type(e))
