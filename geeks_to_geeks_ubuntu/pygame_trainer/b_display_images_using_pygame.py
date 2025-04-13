3dcd1ea045bff554b9887865ac4bec6809e4ef5b
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-13 16:16:45 -0300

    Update b_display_images_using_pygame.py

--- geeks_to_geeks_ubuntu/pygame_trainer/b_display_images_using_pygame.py
+++ geeks_to_geeks_ubuntu/pygame_trainer/b_display_images_using_pygame.py
@@ -1,14 +1,14 @@
-# image (125,125)
+# image (150,150)
 import pygame
 import sys
 
 pygame.init()
 
-screen = pygame.display.set_mode((500,500))
+screen = pygame.display.set_mode((600,600))
 image = pygame.image.load('SONIC.jpeg')
 
 while True:
-    screen.blit(image,(125,125))
+    screen.blit(image, (150,150))
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
