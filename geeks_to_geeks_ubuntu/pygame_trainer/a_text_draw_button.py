c794d3eb09e4dfb0a038350cd39b73261327ae0d
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-13 15:58:24 -0300

    Update a_text_draw_button.py

--- geeks_to_geeks_ubuntu/pygame_trainer/a_text_draw_button.py
+++ geeks_to_geeks_ubuntu/pygame_trainer/a_text_draw_button.py
@@ -1,4 +1,4 @@
-# threea screens
+# Two screens
 
 import pygame
 import sys
@@ -8,11 +8,11 @@ pygame.init()
 screen = pygame.display.set_mode((500,500))
 
 # draw
-draw = pygame.draw.rect(screen, 'red', (50,50,30,30))
+draw = pygame.draw.rect(screen, "blue", (50,50,30,30))
 
-# text
+# read
 font = pygame.font.SysFont('Times New Roman', 25)
-text = font.render("Jesus is the light of the World", True, "pink")
+text = font.render('Jesus is the light of the World!', True, 'red')
 text_rect = text.get_rect()
 text_rect.center=(250,250)
 
@@ -20,13 +20,12 @@ while True:
     screen.blit(text, text_rect)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
-            pygame.quit()
             sys.exit()
+            pygame.quit()
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_0:
-                screen.fill('lightblue')
-            if event.key==pygame.K_1:
                 screen.fill('green')
-            if event.key==pygame.K_2:
-                screen.fill('gray')
+                pygame.draw.line(screen, 'gray', (10,10),(500,500),25)
+            if event.key==pygame.K_1:
+                screen.fill('lightblue')
     pygame.display.flip()
