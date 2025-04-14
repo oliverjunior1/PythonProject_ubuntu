2dfde44d1e5ad201875fc734e004901f5e6f36a8
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-14 16:41:48 -0300

    Update c_make_a_button_in_pygame.py

--- geeks_to_geeks_ubuntu/pygame_trainer/c_make_a_button_in_pygame.py
+++ geeks_to_geeks_ubuntu/pygame_trainer/c_make_a_button_in_pygame.py
@@ -4,10 +4,19 @@ import sys
 pygame.init()
 
 screen = pygame.display.set_mode((500,500))
+pygame.display.set_caption('Moving Rectangle')
+x = 200
+y = 200
+width = 10
+height = 10
+vel = 10
+
 
 while True:
+    pygame.time.delay(10)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             sys.exit()
             pygame.quit()
+    keys = pygame.key.get_pressed()
     pygame.display.flip()
