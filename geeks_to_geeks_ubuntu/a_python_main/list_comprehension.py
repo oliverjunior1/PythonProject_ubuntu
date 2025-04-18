ea717c22349eca0ccda96ac0ca35157b45604cb5
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-18 20:24:18 -0300

    Update list_comprehension.py

--- geeks_to_geeks_ubuntu/a_python_main/list_comprehension.py
+++ geeks_to_geeks_ubuntu/a_python_main/list_comprehension.py
@@ -1,24 +1,14 @@
 # Four numbers  for 3
 # problem 1: Make a list with some family members, that change to a list with name and last name.
-family = ["Joaquim", "Alyne", "Joao", "Mariane"]
+list_names = ["Alyne", "Joaquim", "Mariane", "Joao"]
 
-family_oliver = [x + " Oliveira" for x in family]
+name_lname = [x+" Oliveira" for x in list_names]
 
-print(family_oliver)
+print(name_lname)
 # Withdraw from some list one list with only 'c'
-fruits = ['cacao', 'orange', 'cherry', 'grape', 'crabapple']
 
-fruits_c = [x for x in fruits if 'c' in x]
-
-print(fruits_c)
 
 # multiply all the elements from a list for 5
-
-x = [1,2,3,4,5]
-
-x_5 = [b**5 for b in x]
-
-print(x_5)
 
 
 
