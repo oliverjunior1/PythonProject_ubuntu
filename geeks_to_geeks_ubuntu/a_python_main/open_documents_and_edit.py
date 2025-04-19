644b32ccfd4c3947d411636790378ac311ba5e7a
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-19 19:42:02 -0300

    Update open_documents_and_edit.py

--- geeks_to_geeks_ubuntu/a_python_main/open_documents_and_edit.py
+++ geeks_to_geeks_ubuntu/a_python_main/open_documents_and_edit.py
@@ -1,2 +1,6 @@
-with open('message2.txt', 'w') as file:
-    
+x = open('message2.txt', 'w')
+x.write("Jesus is love, and all the love from the earth, isn't like him love!")
+
+x = open('message2.txt', 'r')
+y = x.read()
+print(y)
