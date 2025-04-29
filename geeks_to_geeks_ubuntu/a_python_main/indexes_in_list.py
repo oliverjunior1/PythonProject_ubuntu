48ff6322bbb69641cd81ff41212d4a748acc6642
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-29 20:05:44 -0300

    Update indexes_in_list.py

--- geeks_to_geeks_ubuntu/a_python_main/indexes_in_list.py
+++ geeks_to_geeks_ubuntu/a_python_main/indexes_in_list.py
@@ -1,12 +1,9 @@
-# indexes four
+# indexes five
 
 
 numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 
-print(numbers[0::2])
-print(numbers[25::-2])
-print(numbers[1::3])
-print(numbers[21::-2])
+print()
 
 
 
