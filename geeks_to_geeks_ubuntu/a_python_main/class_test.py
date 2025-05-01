638ee7a85458d9596b8768c9b739ab04334a8a2a
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-04-30 22:11:22 -0300

    Update class_test.py

--- geeks_to_geeks_ubuntu/a_python_main/class_test.py
+++ geeks_to_geeks_ubuntu/a_python_main/class_test.py
@@ -1,19 +1,15 @@
-# Big Bang
+# Family
 
-class Big_bang:
-    def __init__(self, character, comic_books):
-        self.character = character
-        self.comic_books = comic_books
+class Family:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
     def __str__(self):
-        return f"The character {self.character} have {self.comic_books} comic books in his bedroom."
+        return f"The name is {self.name} and the age is {self.age} years old."
 
-big_bang = Big_bang('Sheldon', 25000)
-big_bang2 = Big_bang("Leonard", 20000)
-big_bang3 = Big_bang('Howard', 15000)
-big_bang4 = Big_bang('Hajesh', 8008)
+member1 = Family("Joao Pedro", 12)
+member2 = Family("Mariane Vitória", 4)
 
-print(big_bang)
-print(big_bang2)
-print(big_bang3)
-print(big_bang4)
+print(member1)
+print(member2)
