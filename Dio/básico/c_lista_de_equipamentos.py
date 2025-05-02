09680ca632f967b45e06bd9cf0b8edba2207421a
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-05-02 19:15:02 -0300

    Create c_lista_de_equipamentos.py

---
+++ Dio/básico/c_lista_de_equipamentos.py
@@ -0,0 +1,20 @@
+# TODO: Crie uma Lista 'itens' para armazenar os equipamentos:
+lista_de_equipamentos = []
+
+# TODO: Crie um loop para solicitar os itens ao usuário:
+i = 0
+while i < 3:
+    lista = input()
+    # TODO: Solicite o item e armazena na variável "item":
+    lista_de_equipamentos.append(lista)
+    i += 1
+# TODO: Adicione o item à lista "itens":
+# print("Lista de Equipamentos:")
+print("Lista de Equipamentos:")
+
+# Exibe a lista de itens
+
+for itens in lista_de_equipamentos:
+    print(f"- {itens}")
+
+
