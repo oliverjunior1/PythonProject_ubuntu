6118dd218132a647b0b336f52880ba83fa451a21
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-05-05 16:24:31 -0300

    Update basic_iterator.py

--- geeks_to_geeks_ubuntu/a_python_main/iterators/basic_iterator.py
+++ geeks_to_geeks_ubuntu/a_python_main/iterators/basic_iterator.py
@@ -1,8 +1,7 @@
 name = "Joaquim"
+
 it = iter(name)
 
-person = {"name":"Joaquim", "age":40}
-
 print(next(it))
 print(next(it))
 print(next(it))
@@ -10,8 +9,3 @@ print(next(it))
 print(next(it))
 print(next(it))
 print(next(it))
-
-it2 = iter(person)
-
-print(next(it2))
-print(next(it2))
