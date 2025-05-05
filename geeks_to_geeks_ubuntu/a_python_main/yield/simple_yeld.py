60e900e6b90abe7302b9e6a935352517d07bd7d2
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-05-05 19:20:58 -0300

    Update simple_yeld.py

--- geeks_to_geeks_ubuntu/a_python_main/yield/simple_yeld.py
+++ geeks_to_geeks_ubuntu/a_python_main/yield/simple_yeld.py
@@ -1,9 +1,9 @@
-def next_yield():
+def next_yeld():
     i = 1
     while True:
         yield i*i
-        i += 1
-for x in next_yield():
+        i+=1
+for x in next_yeld():
     if x >100:
         break
     else:
