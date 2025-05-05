3a98daca51d762c3799b6f8fcddfc2b0c6871edd
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-05-05 16:00:18 -0300

    Create basic_iterator.py

---
+++ geeks_to_geeks_ubuntu/a_python_main/iterators/basic_iterator.py
@@ -0,0 +1,20 @@
+x = [1,2,3,4,5,6,7]
+name = "Joaquim"
+it = iter(x)
+
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+
+it_2 = iter(name)
+print(next(it_2))
+print(next(it_2))
+print(next(it_2))
+print(next(it_2))
+print(next(it_2))
+print(next(it_2))
+print(next(it_2))
