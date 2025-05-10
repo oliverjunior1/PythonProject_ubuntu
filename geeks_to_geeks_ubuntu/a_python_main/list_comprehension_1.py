41511414e6680b20d9f17c0bd3df15e81607ccde
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-05-10 20:40:38 -0300

    Update list_comprehension_1.py

--- geeks_to_geeks_ubuntu/a_python_main/list_comprehension_1.py
+++ geeks_to_geeks_ubuntu/a_python_main/list_comprehension_1.py
@@ -1,11 +1,11 @@
-# Four numbers  for 3
+
 # problem 1: Make a list with some family members, that change to a list with name and last name.
-family_list = ["Alyne", "Joaquim", "Joao", "Mariane"]
 
-names = [x + " Oliveira" for x in family_list]
+name = ["Joaquim", "Alyne", "Joao", "Mariane"]
 
-print(names)
+name_lname = [x + ' Oliveira' for x in name]
 
+print(name_lname)
 
 
 
