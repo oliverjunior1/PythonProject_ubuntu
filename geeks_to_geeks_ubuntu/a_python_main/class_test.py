cd074382c608b809d9e2ae21082fb5c9aca11f4f
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-05-10 20:24:16 -0300

    Update class_test.py

--- geeks_to_geeks_ubuntu/a_python_main/class_test.py
+++ geeks_to_geeks_ubuntu/a_python_main/class_test.py
@@ -1,15 +1,19 @@
-# two_and_a_half
+# Big Bang
 
-class Two_and_a_half_man:
-    def __init__(self, name, womans):
-        self.name = name
-        self.womans = womans
+class Big_Bang:
+    def __init__(self, character, comic_book):
+        self.character = character
+        self.comic_book = comic_book
 
     def __str__(self):
-        return f"The character {self.name} has {self.womans} womans in his bedroom."
+        return f"The character has {self.character} has {self.comic_book} in his bedroom."
 
-character1 = Two_and_a_half_man("Charlie", 5)
-character2 = Two_and_a_half_man("Alan", 0)
+character1 = Big_Bang("Sheldon", 25000)
+character2 = Big_Bang("Leonard", 18000)
+character3 = Big_Bang("Howard", 12012)
+character4 = Big_Bang("Hajesh", 8008)
 
 print(character1)
 print(character2)
+print(character3)
+print(character4)
