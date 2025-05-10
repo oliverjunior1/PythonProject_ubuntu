c58c8162dabf7b00878a0d50d417aac37fca64d3
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-05-10 19:43:28 -0300

    Update method_decorator.py

--- geeks_to_geeks_ubuntu/a_python_main/decorations/method_decorator.py
+++ geeks_to_geeks_ubuntu/a_python_main/decorations/method_decorator.py
@@ -1,18 +1,2 @@
-def fun1(x):
-    def fun2(*args, **kwargs):
-        print("######winner########")
-        x(*args, **kwargs)
-        print("####################")
-        print()
-    return fun2
-
-@fun1
-def greetings(name, age):
-    print(f"{name} and {age} years old")
-
-greetings("Joaquim", 40)
-greetings("Alyne", 38)
-greetings("Joao", 12)
-greetings("Mariane", 4)
 
 
