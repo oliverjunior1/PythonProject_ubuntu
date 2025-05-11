31fbb19073b3b4ea16bf14be1a6a0920afb40330
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-05-10 23:28:39 -0300

    Update mega_sena.py

--- geeks_to_geeks_ubuntu/a_python_main/mega_sena.py
+++ geeks_to_geeks_ubuntu/a_python_main/mega_sena.py
@@ -1,11 +1,11 @@
-# lotofacil
+# mega
 import random
 
-def lotofacil():
-    x = sorted(random.sample(range(1,26),15))
+def mega():
+    x = sorted(random.sample(range(1,61),6))
     return print(x)
 
-lotofacil()
+mega()  
 
 
 
